cbd1e7ba4eaf4e34e6481d4ab42491042f6c7fb5
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-06 21:18:21 +0100

    Write controller for getting films

--- backend/controllers/filmControllers.js
+++ backend/controllers/filmControllers.js
@@ -1,7 +1,20 @@
 const Film = require("../models/film");
 
-// TODO: consider not necessarily wanting to get every single film document in a game and 
-// how that might impact how you right the controllers/routes
-exports.getFilms = (req, res, next) => {
-    res.send("TODO: implement getFilms controller");
+exports.getFilms = (req, res) => {
+    const numberOfRandomFilms = parseInt(req.query.numberOfRandomFilms);
+
+    const getRandomFilmsAggregate = (filmQuantity) => Film.aggregate().sample(filmQuantity);
+    const getAllFilms = () => Film.find();
+    const respondWithFilmsAsJSON = (films) => res.json(films);
+    const respondWithServerError = (error) => res.status(503).json({error})
+
+    if (numberOfRandomFilms) {
+        getRandomFilmsAggregate(numberOfRandomFilms)
+            .then(respondWithFilmsAsJSON)
+            .catch(respondWithServerError);
+    } else {
+        getAllFilms()
+            .then(respondWithFilmsAsJSON)
+            .catch(respondWithServerError);
+    }
 }
