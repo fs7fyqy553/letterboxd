3a9bd167ea8170b93b4829d8474dc88d28c623dd
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-06 17:04:29 +0100

    Write module for film schema

---
+++ backend/models/film.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: consider adding more requirements to the schema definition
+const FilmSchema = new Schema({
+    filmTitle: {type: String, required: true},
+    releaseYearString: {type: String, required: true},
+    directorNameArray: {type: Array, required: true},
+    averageRatingString: {type: String, required: true},
+    filmPosterURL: {type: String, required: true},
+});
+
+module.exports = mongoose.model("Film", FilmSchema);
