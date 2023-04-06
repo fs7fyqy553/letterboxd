7b60cfff1c42e7f5a40a0a02fd44ff960e0d9fb5
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-06 14:56:25 +0100

    Setup backend skeleton
    - Run Express Application Generator
    - Remove unnecessary packages from resulting package.json and code
    - Remove unnecessary boilerplate
    - Install dependencies listed in package.json
    - Install nodemon as a development dependency
    - Write a couple of commands in package.json for convenience

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+module.exports = app;
