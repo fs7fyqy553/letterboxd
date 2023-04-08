3e3eb6955cd5ee5e11778d449577fbb11f8c02d3
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-08 12:59:53 +0100

    Render CurrentScore in App, passing dummy score in props

--- frontend/web-game/src/App.js
+++ frontend/web-game/src/App.js
@@ -1,10 +1,14 @@
 import HighScore from "./components/HighScore";
+import CurrentScore from "./components/CurrentScore";
 
 function App() {
   return (
     <div className="App">
       <HighScore
         highScore={0}
+      />
+      <CurrentScore
+        currentScore={0}
       />
     </div>
   );
---
+++ frontend/web-game/src/components/CurrentScore.js
@@ -0,0 +1,9 @@
+function CurrentScore({ currentScore }) {
+    return (
+        <div className="CurrentScore">
+            Current Score: {currentScore}
+        </div>
+    )
+}
+
+export default CurrentScore;
