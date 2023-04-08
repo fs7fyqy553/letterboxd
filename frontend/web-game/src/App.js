a28faa729f28aef0aa1bd695368ef60c4bde133d
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-08 12:56:46 +0100

    Render HighScore in App, passing dummy score in props

--- frontend/web-game/src/App.js
+++ frontend/web-game/src/App.js
@@ -1,6 +1,11 @@
+import HighScore from "./components/HighScore";
+
 function App() {
   return (
     <div className="App">
+      <HighScore
+        highScore={0}
+      />
     </div>
   );
 }
---
+++ frontend/web-game/src/components/HighScore.js
@@ -0,0 +1,9 @@
+function HighScore({ highScore }) {
+    return (
+        <div className="HighScore">
+            High Score: {highScore}
+        </div>
+    );
+}
+
+export default HighScore;
