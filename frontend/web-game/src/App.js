786a9e5d57078a144e5ac22c2a1176ebd064cf1a
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-08 19:20:45 +0100

    Set initial state of filmDetailsObjectToGuess
    Make it null (because call to get filmDetailsObject asynchronous and
    will first be done when component has mounted)
    
    Pass filmDetailsObjectToGuess down to FilmToGuess
    
    Render FilmToGuess only when filmDetailsObjectToGuess is not null,
    since FilmDetails in FilmToGuess will not be made properly otherwise;
    could conditionally render said FilmDetails instead but will later add
    onClick to FilmToGuess, which shouldn't be allowed to work if film

--- frontend/web-game/src/App.js
+++ frontend/web-game/src/App.js
@@ -8,6 +8,7 @@ function App() {
   const [highScore, setHighScore] = useState(0);
   const [currentScore, setCurrentScore] = useState(0);
   const [referenceFilmDetailsObject, setReferenceFilmDetailsObject] = useState(null);
+  const [filmDetailsObjectToGuess, setFilmDetailsObjectToGuess] = useState(null);
   return (
     <div className="App">
       <HighScore
@@ -21,17 +22,11 @@ function App() {
           filmDetailsObject={referenceFilmDetailsObject}
         />
       }
-      <FilmToGuess
-        filmDetailsObject={
-          {
-            filmTitle: "Film 2",
-            releaseYearString: "Year 2",
-            directorNameArray: ["Director 3", "Director 4"],
-            averageRatingString: "3.7",
-            filmPosterURL: "https://blogs.kcl.ac.uk/editlab/files/2016/12/film-1155439_960_720.jpg",
-          }
-        }
-      />
+      {filmDetailsObjectToGuess &&
+        <FilmToGuess
+          filmDetailsObject={filmDetailsObjectToGuess}
+        />
+      }
     </div>
   );
 }
