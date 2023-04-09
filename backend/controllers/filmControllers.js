f3caf42a34fd50041675da56a40555f2b41ee48c
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-09 14:26:25 +0100

    Enable getting of two random films with different average ratings
    TODO: amend front-end API calls to this

--- backend/controllers/filmControllers.js
+++ backend/controllers/filmControllers.js
@@ -1,15 +1,32 @@
+// TODO: clean up file structure
 const Film = require("../models/film");
 
 exports.getFilms = (req, res) => {
-    const numberOfRandomFilms = parseInt(req.query.numberOfRandomFilms);
+    const twoFilmsWithDifferentRatings = req.query.twoFilmsWithDifferentRatings;
 
-    const getRandomFilmsAggregate = (filmQuantity) => Film.aggregate().sample(filmQuantity);
+    // TODO: add error handling for when required films aren't found
+    const getRandomFilmWithoutThisAverageRatingString = (avoidedAverageRatingString) => {
+        return candidatesAggregate = Film.aggregate([
+            { $match: { averageRatingString: { $ne: avoidedAverageRatingString }}},
+        ])
+        .sample(1)
+        .then(([film]) => film);
+    }
+    const getRandomFilm = () => {
+        return Film.aggregate()
+            .sample(1)
+            .then(([film]) => film);
+    };
+    const getTwoFilmsWithDifferentRatings = async () => {
+        const firstFilm = await getRandomFilm();
+        const secondFilm = await getRandomFilmWithoutThisAverageRatingString(firstFilm.averageRatingString);
+        return [firstFilm, secondFilm];
+    };
     const getAllFilms = () => Film.find();
     const respondWithFilmsAsJSON = (films) => res.json(films);
     const respondWithServerError = (error) => res.status(503).json({error})
-
-    if (numberOfRandomFilms) {
-        getRandomFilmsAggregate(numberOfRandomFilms)
+    if (twoFilmsWithDifferentRatings === "true") {
+        getTwoFilmsWithDifferentRatings()
             .then(respondWithFilmsAsJSON)
             .catch(respondWithServerError);
     } else {
