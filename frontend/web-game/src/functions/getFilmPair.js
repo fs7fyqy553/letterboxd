55eaaa923424922e1a847c12f1047a50da863f03
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-11 18:35:19 +0100

    Make variables more clear

--- frontend/web-game/src/functions/getFilmPair.js
+++ frontend/web-game/src/functions/getFilmPair.js
@@ -1,9 +1,9 @@
 async function getFilmPair() {
     // NOTE: the two selected films should have different ratings
     try {
-        const response = await fetch("http://localhost:3000/api/films?twoFilmsWithDifferentRatings=true");
-        const data = await response.json();
-        return data;
+        const filmArrayJSON = await fetch("http://localhost:3000/api/films?twoFilmsWithDifferentRatings=true");
+        const filmArray = await filmArrayJSON.json();
+        return filmArray;
     } catch(err) {
         console.log(err);
     }
