125a100b57c9af5933597471bab1ab9415a7ed74
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-12 12:31:21 +0100

    Format file

--- backend/mongoConfig.js
+++ backend/mongoConfig.js
@@ -1,7 +1,7 @@
 // TODO: consider that you might be able to use await in here if this is just a module
-require("dotenv").config();
-const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
 
 mongoose.connect(process.env.DEV_CONNECTION_STRING);
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "MongoDB connection error"));
+db.on('error', console.error.bind(console, 'MongoDB connection error'));
