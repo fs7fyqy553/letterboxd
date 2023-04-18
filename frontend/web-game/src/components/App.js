385ca93d5c06cf9d10fafc68f4ceb1fb3395de90
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-18 21:01:31 +0100

    Link to GitHub repo

--- frontend/web-game/src/components/App.js
+++ frontend/web-game/src/components/App.js
@@ -1,11 +1,18 @@
 import "../styles/App.css";
 import Game from "./Game";
+import invertocat from "../logos/github-mark-white.svg";
 
 function App() {
     return (
         <div className="App">
             <Game/>
-            <div className="Footer">Made by James Graça-Jones</div>
+            {/* <div className="Footer">Made by James Graça-Jones</div> */}
+            <div className="Footer">
+                {/* <a href="https://github.com/JamesGJ5/letterxbod-higher-or-lower">Code</a> */}
+                <a href="https://github.com/JamesGJ5/letterxbod-higher-or-lower">
+                    <img src={invertocat}/>
+                </a>
+            </div>
         </div>
     )
 }
