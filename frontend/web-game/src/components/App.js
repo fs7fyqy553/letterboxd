30cbda0e6f1b3c32071df3f9a377ed68b5808657
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-19 12:11:00 +0100

    Add link to LinkedIn profile

--- frontend/web-game/src/components/App.js
+++ frontend/web-game/src/components/App.js
@@ -1,16 +1,18 @@
 import "../styles/App.css";
 import Game from "./Game";
 import invertocat from "../logos/github-mark-white.svg";
+import inLogo from "../logos/LI-In-Bug.png";
 
 function App() {
     return (
         <div className="App">
             <Game/>
-            {/* <div className="Footer">Made by James Graça-Jones</div> */}
             <div className="Footer">
-                {/* <a href="https://github.com/JamesGJ5/letterxbod-higher-or-lower">Code</a> */}
                 <a href="https://github.com/JamesGJ5/letterxbod-higher-or-lower">
                     <img src={invertocat}/>
+                </a>
+                <a href="https://www.linkedin.com/in/james-graca-jones/">
+                    <img src={inLogo}/>
                 </a>
             </div>
         </div>
