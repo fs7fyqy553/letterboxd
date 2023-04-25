b69692af267d7670237a4c75e39e968abbf65296
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-25 16:39:11 +0100

    Remove unused imports

--- frontend/web-game/src/components/Game.test.js
+++ frontend/web-game/src/components/Game.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { findAllByTestId, render, screen, waitFor, act } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Game from "./Game";
 
