f5757f6e0a4b40ba6287bb6a934e357508d2e616
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-27 11:36:18 +0100

    Load production connection string
    Note: this will not work locally, the production connection string
    will be stored where the server is hosted

--- backend/server/database/mongoConfig.js
+++ backend/server/database/mongoConfig.js
@@ -1,6 +1,6 @@
 require('dotenv').config({ path: `${__dirname}/../.env` });
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DEV_CONNECTION_STRING);
+mongoose.connect(process.env.PRODUCTION_CONNECTION_STRING || process.env.DEV_CONNECTION_STRING);
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error'));
