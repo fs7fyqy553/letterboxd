204ce4d6cdcdaf3869f4e1fda4a7c0913c9e3347
James Graca-Jones <jamesgracajones@gmail.com>
2023-04-27 17:31:31 +0100

    Enable requests to API from anywhere for now

--- backend/server/routes/APIRouter.js
+++ backend/server/routes/APIRouter.js
@@ -1,7 +1,9 @@
 const { Router } = require('express');
+const cors = require('cors');
 const filmRouter = require('./filmRouter');
 
 const APIRouter = Router();
+APIRouter.use(cors());
 APIRouter.use('/films', filmRouter);
 
 module.exports = APIRouter;
--- backend/server/routes/filmRouter.js
+++ backend/server/routes/filmRouter.js
@@ -1,16 +1,8 @@
 const { Router } = require('express');
-const cors = require('cors');
 const filmControllers = require('../controllers/filmControllers');
 
 const filmRouter = Router();
 
-filmRouter.options(
-  '/',
-  cors({
-    origin: 'http://localhost:3006',
-  })
-);
-
-filmRouter.get('/', cors(), filmControllers.getFilms);
+filmRouter.get('/', filmControllers.getFilms);
 
 module.exports = filmRouter;
