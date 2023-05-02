b421edc28a08b64c38fd23c5fbca8e9e87fe4e70
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-02 18:00:55 +0100

    Configure CORS only on API routes

--- backend/server/app.js
+++ backend/server/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
-const cors = require('cors');
 require('./database/mongoConfig');
 const APIRouter = require('./routes/APIRouter');
 
 const app = express();
-app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
--- backend/server/routes/APIRouter.js
+++ backend/server/routes/APIRouter.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-// const cors = require('cors');
-const authControllers = require('../controllers/authControllers');
+const cors = require('cors');
+// const authControllers = require('../controllers/authControllers');
 const filmRouter = require('./filmRouter');
 
 const APIRouter = Router();
-// APIRouter.use(cors);
+APIRouter.use(cors());
 // APIRouter.use(authControllers.authenticateAPICall);
 APIRouter.use('/films', filmRouter);
 
