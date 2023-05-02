92c5ad0c52a8f0b9039f79848414f6efb3a64227
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-02 15:33:11 +0100

    Try different cors setup

--- backend/server/routes/APIRouter.js
+++ backend/server/routes/APIRouter.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const authControllers = require('../controllers/authControllers');
 const filmRouter = require('./filmRouter');
 
 const APIRouter = Router();
-APIRouter.use(cors({ origin: 'https://letterboxd-guessing-game.up.railway.app/' }));
+// APIRouter.use(cors({ origin: 'https://letterboxd-guessing-game.up.railway.app/' }));
 // APIRouter.use(authControllers.authenticateAPICall);
 APIRouter.use('/films', filmRouter);
 
--- backend/server/routes/filmRouter.js
+++ backend/server/routes/filmRouter.js
@@ -1,8 +1,16 @@
 const { Router } = require('express');
+const cors = require('cors');
 const filmControllers = require('../controllers/filmControllers');
 
 const filmRouter = Router();
 
-filmRouter.get('/', filmControllers.getFilms);
+filmRouter.options(
+  '/',
+  cors({
+    origin: 'https://letterboxd-guessing-game.up.railway.app/',
+  })
+);
+
+filmRouter.get('/', cors(), filmControllers.getFilms);
 
 module.exports = filmRouter;
