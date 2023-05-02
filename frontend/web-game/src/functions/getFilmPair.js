510ce51c536bce7586a0952921f7189c145ef54f
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-02 14:58:44 +0100

    Enable cors

--- frontend/web-game/src/functions/getFilmPair.js
+++ frontend/web-game/src/functions/getFilmPair.js
@@ -2,8 +2,11 @@ async function getFilmPair() {
   // NOTE: the two selected films should have different ratings
   try {
     const responseJSON = await fetch(
-      "https://letterxbod-guessing-game-production.up.railway.app/api/films?twoFilmsWithDifferentRatings=true"
-      // "http://localhost:8000/getFilmPair"
+      "https://letterxbod-guessing-game-production.up.railway.app/api/films?twoFilmsWithDifferentRatings=true",
+      // "http://localhost:8000/getFilmPair",
+      {
+        mode: "cors",
+      }
     );
     const responseObject = await responseJSON.json();
     const filmArray = responseObject.films;
