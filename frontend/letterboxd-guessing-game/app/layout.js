f790356230f5405abec525ef9848b0c386099f32
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-04 18:03:18 +0100

    Add TODO

--- frontend/letterboxd-guessing-game/app/layout.js
+++ frontend/letterboxd-guessing-game/app/layout.js
@@ -5,6 +5,9 @@ import "./styles/index.css";
 
 // const inter = Inter({ subsets: ['latin'] })
 
+// TODO: try to make sure the following (from CRA version) are implemented:
+// <meta name="viewport" content="width=device-width, initial-scale=1" />
+// <meta charset="utf-8" />
 export const metadata = {
   title: 'Letterboxd Guessing Game',
   description: 'Game made using NextJS',
