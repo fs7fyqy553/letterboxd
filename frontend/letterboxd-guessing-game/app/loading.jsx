7257a6d90a33b65f722cd232bb745eda1d1dbc35
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-05 16:30:31 +0100

    Write content to display while page loads

---
+++ frontend/letterboxd-guessing-game/app/loading.jsx
@@ -0,0 +1,9 @@
+function LoadingPage() {
+    // TODO: ensure loading is accessible and use a spinner
+    return (
+        <p>
+            Loading Film Data...
+        </p>
+    );
+}
+export default LoadingPage;
