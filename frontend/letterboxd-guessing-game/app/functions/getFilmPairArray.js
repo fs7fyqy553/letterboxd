7bc3d2d1d0c14b219388c00f7c548c5c7ef9c3f4
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-05 14:04:03 +0100

    Write function for getting chosen number of film pairs from API

---
+++ frontend/letterboxd-guessing-game/app/functions/getFilmPairArray.js
@@ -0,0 +1,16 @@
+async function getFilmPairArray(numberOfPairs) {
+  // NOTE: the two selected films should have different ratings
+  const requestURL = process.env.PUBLIC_API_URL || process.env.DEVELOPMENT_API_URL;
+  try {
+    const responseJSON = await fetch(
+      `${requestURL}&numberOfPairs=${numberOfPairs}`
+    );
+    const responseObject = await responseJSON.json();
+    const filmPairArray = responseObject.films;
+    return filmPairArray;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export default getFilmPairArray;
