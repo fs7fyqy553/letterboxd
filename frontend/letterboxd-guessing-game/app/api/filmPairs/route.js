99fd8898148b5facf3b9a6c33eca572443a9aae1
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-06 16:14:13 +0100

    Remove console.log statement

--- frontend/letterboxd-guessing-game/app/api/filmPairs/route.js
+++ frontend/letterboxd-guessing-game/app/api/filmPairs/route.js
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import getFilmPairArray from "@/app/functions/getFilmPairArray";
 
 export async function GET(request) {
-    console.log(request.url);
     const { searchParams } = new URL(request.url);
     const numberOfPairs = searchParams.get("numberOfPairs");
     const filmPairArray = await getFilmPairArray(numberOfPairs);
