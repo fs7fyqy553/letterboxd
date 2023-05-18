a3b4ba0c75a3f71998d526bb842951d8ebd06ba6
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-18 13:42:14 +0100

    Remove width requirement from media query

--- frontend/app/components/FilmPoster.jsx
+++ frontend/app/components/FilmPoster.jsx
@@ -1,7 +1,7 @@
 import useMediaQuery from "@mui/material/useMediaQuery";
 
 function FilmPoster({ filmPosterURL, filmBackdropImageURL }) {
-  const useLandscapeImage = useMediaQuery("(max-width: 800px) and (max-height: 400px)");
+  const useLandscapeImage = useMediaQuery("(max-height: 400px)");
   const backgroundImageURL = (useLandscapeImage) ? filmBackdropImageURL : filmPosterURL;
   return (
     <div className="img" style={{ backgroundImage: `url(${backgroundImageURL})` }} />
