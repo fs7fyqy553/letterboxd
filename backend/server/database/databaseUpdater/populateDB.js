c76dfc749390549e76a016c8277f7b55cce71ade
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-19 20:34:40 +0100

    Give cron job a 5-minute time limit

--- backend/server/database/databaseUpdater/populateDB.js
+++ backend/server/database/databaseUpdater/populateDB.js
@@ -2,6 +2,7 @@ import '../mongoConfig.js';
 import processFilmsInList from '../../scraping/dist/index.js';
 import Film from '../../models/film.js';
 import { CronJob } from 'cron';
+import { timeout, TimeoutError } from 'promise-timeout';
 
 function updateAverageRatingString(existingFilmDoc, newAverageRatingString) {
   const updatedFilmDoc = existingFilmDoc;
@@ -38,14 +39,23 @@ async function saveScrapedFilmDetailsObject(filmDetailsObject) {
   }
 }
 
-const job = new CronJob({
-  cronTime: '0 0 * * *',
-  onTick: async () => {
-    await processFilmsInList(
+async function scrapePopularFilmsList() {
+  try {
+    const processPromise = processFilmsInList(
       'https://letterboxd.com/bucksalypse/list/letterboxd-500-most-watched-movies-of-all/',
       saveScrapedFilmDetailsObject
     );
-  },
+    await timeout(processPromise, 300);
+  } catch (err) {
+    if (err instanceof TimeoutError) {
+      console.log('Process timed out');
+    }
+  }
+}
+
+const job = new CronJob({
+  cronTime: '0 0 * * *',
+  onTick: scrapePopularFilmsList,
   timeZone: 'Europe/London',
 });
 
