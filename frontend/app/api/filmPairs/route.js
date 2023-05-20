160ff65c77ee94da7b2fff2a014b386154ba889a
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-20 16:52:38 +0100

    Split URL logic into separate function

--- frontend/app/api/filmPairs/route.js
+++ frontend/app/api/filmPairs/route.js
@@ -1,9 +1,13 @@
 import { NextResponse } from "next/server";
 import getFilmPairArray from "@/app/functions/getFilmPairArray";
 
-export async function GET(request) {
+function getRequestedNumberOfPairs(request) {
     const { searchParams } = new URL(request.url);
-    const numberOfPairs = searchParams.get("numberOfPairs");
+    return searchParams.get("numberOfPairs");  
+}
+
+export async function GET(request) {
+    const numberOfPairs = getRequestedNumberOfPairs(request);
     const filmPairArray = await getFilmPairArray(numberOfPairs);
     return NextResponse.json(filmPairArray);
 }
