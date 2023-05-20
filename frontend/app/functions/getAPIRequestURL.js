e7e9fdcdfb9905d67c6767779c43738fad6b2499
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-20 17:05:32 +0100

    Put function into file where it's used

--- frontend/app/functions/getAPIRequestURL.js
+++
@@ -1,6 +0,0 @@
-function getAPIRequestURL(requestPath) {
-    const API_URL = process.env.PUBLIC_API_URL || process.env.DEVELOPMENT_API_URL;
-    return API_URL + requestPath;
-}
-
-export default getAPIRequestURL;
--- frontend/app/functions/getFilmPairArray.js
+++ frontend/app/functions/getFilmPairArray.js
@@ -1,4 +1,7 @@
-import getAPIRequestURL from "./getAPIRequestURL";
+function getAPIRequestURL(requestPath) {
+    const API_URL = process.env.PUBLIC_API_URL || process.env.DEVELOPMENT_API_URL;
+    return API_URL + requestPath;
+}
 
 function fetchFilmPairArray(numberOfPairs) {
   const requestURL = getAPIRequestURL(`/films?twoFilmsWithDifferentRatings=true&numberOfPairs=${numberOfPairs}`);
