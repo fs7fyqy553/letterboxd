225781a57d3a187b5357028b58b88e7878b2d95c
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-20 16:45:27 +0100

    Make JSX returned by component more clear

--- frontend/app/components/FilmPoster/FilmPoster.js
+++ frontend/app/components/FilmPoster/FilmPoster.js
@@ -3,6 +3,7 @@ import useMediaQuery from "@mui/material/useMediaQuery";
 function FilmPoster({ portraitModeURL, landscapeModeURL }) {
   const useLandscapeImage = useMediaQuery("(max-height: 400px)");
   const backgroundImageURL = (useLandscapeImage) ? landscapeModeURL : portraitModeURL;
+
   return (
     <div className="img" style={{ backgroundImage: `url(${backgroundImageURL})` }} />
   );
