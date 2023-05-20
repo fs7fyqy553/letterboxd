4760de765ab7bfc701e04b32666e97209b934af9
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-20 18:28:53 +0100

    Space out code for clarity

--- backend/server/routes/APIRouter.js
+++ backend/server/routes/APIRouter.js
@@ -4,8 +4,10 @@ const authControllers = require('../controllers/authControllers');
 const filmRouter = require('./filmRouter');
 
 const APIRouter = Router();
+
 APIRouter.use(cors());
 APIRouter.use(authControllers.authenticateAPICall);
+
 APIRouter.use('/films', filmRouter);
 
 module.exports = APIRouter;
