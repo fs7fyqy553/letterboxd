c482212416ea1a7b89d3f611be9eeadf80e193ce
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-20 18:44:59 +0100

    Remove console statements

--- backend/server/tests/mongoConfigTesting.js
+++ backend/server/tests/mongoConfigTesting.js
@@ -8,14 +8,9 @@ async function initializeMongoServer() {
   mongoose.connect(mongoUri);
 
   mongoose.connection.on('error', (e) => {
-    console.log(e);
     if (e.message.code === 'ETIMEDOUT') {
       mongoose.connect(mongoUri);
     }
-  });
-
-  mongoose.connection.once('open', () => {
-    console.log(`MongoDB successfully connected to ${mongoUri}`);
   });
 }
 
