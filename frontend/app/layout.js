6761918c680e3d5431bbc5db255cb4523c28b9a6
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-20 14:21:56 +0100

    Clean up comments

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -4,13 +4,9 @@ import { Roboto } from "next/font/google";
 const roboto = Roboto({
   weight: ["400"],
   style: ["italic"],
-  // TODO: revise the choice of font subset
   subsets: ["latin"],
 })
 
-// TODO: try to make sure the following (from CRA version) are implemented:
-// <meta name="viewport" content="width=device-width, initial-scale=1" />
-// <meta charset="utf-8" />
 export const metadata = {
   title: 'Letterboxd Guessing Game',
   description: 'Game made using NextJS',
