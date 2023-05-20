9d11ca59fe0608a794a90cf548ce1fc93255303b
James Graca-Jones <jamesgracajones@gmail.com>
2023-05-20 14:34:42 +0100

    Clean up comment

--- frontend/app/loading.jsx
+++ frontend/app/loading.jsx
@@ -1,9 +1,6 @@
 function LoadingPage() {
-    // TODO: ensure loading is accessible and use a spinner
     return (
-        <p>
-            Loading Letterboxd Guessing Game...
-        </p>
+        <p>Loading Letterboxd Guessing Game...</p>
     );
 }
 export default LoadingPage;
