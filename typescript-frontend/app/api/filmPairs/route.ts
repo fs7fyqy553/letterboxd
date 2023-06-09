87be69e2f603598c42cc6d63b5e45ccaf08b6a43
James Graca-Jones <jamesgracajones@gmail.com>
2023-06-09 19:50:23 +0100

    Convert to TS

--- typescript-frontend/app/api/filmPairs/route.ts
+++ typescript-frontend/app/api/filmPairs/route.ts
@@ -1,11 +1,12 @@
+import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-function getAPIRequestURL(requestPath) {
+function getAPIRequestURL(requestPath: string): string {
     const API_URL = process.env.PUBLIC_API_URL || process.env.DEVELOPMENT_API_URL;
     return API_URL + requestPath;
 }
 
-function fetchFilmPairArray(numberOfPairs) {
+function fetchFilmPairArray(numberOfPairs: string): Promise<Response> {
   const requestURL = getAPIRequestURL(`/films?twoFilmsWithDifferentRatings=true&numberOfPairs=${numberOfPairs}`);
   return fetch(
     requestURL,
@@ -18,23 +19,24 @@ function fetchFilmPairArray(numberOfPairs) {
   );
 }
 
-async function getFilmPairArray(numberOfPairs) {
+async function getFilmPairArray(numberOfPairs: string): Promise<Array<Array<object>>> {
   try {
     const filmPairArrayFetchResponse = await fetchFilmPairArray(numberOfPairs);
     const responseObject = await filmPairArrayFetchResponse.json();
     const filmPairArray = responseObject.films;
+    console.log(filmPairArray);
     return filmPairArray;
   } catch (err) {
     console.log(err);
   }
 }
 
-function getRequestedNumberOfPairs(request) {
+function getRequestedNumberOfPairs(request: NextRequest): string {
     const { searchParams } = new URL(request.url);
     return searchParams.get("numberOfPairs");  
 }
 
-export async function GET(request) {
+export async function GET(request: NextRequest) {
     const numberOfPairs = getRequestedNumberOfPairs(request);
     const filmPairArray = await getFilmPairArray(numberOfPairs);
     return NextResponse.json(filmPairArray);
