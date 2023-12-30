62d8412196b99f9e896e2bc8f0fe4b301943246a
James Graca-Jones <jamesgracajones@gmail.com>
2023-12-30 22:19:46 +0000

    Run npm audit fix for security

--- backend/src/database/mongo-config.ts
+++ backend/src/database/mongo-config.ts
@@ -3,4 +3,7 @@ import mongoose from 'mongoose';
 
 dotenv.config({ path: '.env' });
 
+console.log("Hi");
+console.log(process.env.PRODUCTION_CONNECTION_STRING || process.env.DEV_CONNECTION_STRING)
+
 mongoose.connect(process.env.PRODUCTION_CONNECTION_STRING || process.env.DEV_CONNECTION_STRING);
